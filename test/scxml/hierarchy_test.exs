a6d533253b23fc3f9cef704e670ff56db19cff53
JohnnyT <ubergeek3141@gmail.com>
2019-08-30 18:33:00 -0600

    Adds more hierarchy tests

--- test/scxml/hierarchy_test.exs
+++ test/scxml/hierarchy_test.exs
@@ -19,21 +19,39 @@ defmodule HierarchyTest do
     end)
   end
 
-  #test "scxml test basic1" do
-  #  scxml_path = "./test/fixtures/scxml/basic/basic1.scxml"
-  #  test_path = "./test/fixtures/scxml/basic/basic1.json"
-  #  {:ok, test_contents} = File.read Path.expand test_path
-  #  {:ok, test_config} = Poison.decode test_contents
+  test "scxml test hier1" do
+    scxml_path = "./test/fixtures/scxml/hierarchy/hier1.scxml"
+    test_path = "./test/fixtures/scxml/hierarchy/hier1.json"
+    {:ok, test_contents} = File.read Path.expand test_path
+    {:ok, test_config} = Poison.decode test_contents
 
-  #  machine = Exstate.machine_from_file(scxml_path)
-  #  configuration_literal = machine
-  #    |> Exstate.Machine.configuration_literal
+    machine = Exstate.machine_from_file(scxml_path)
+    configuration_literal = machine
+      |> Exstate.Machine.configuration_literal
 
-  #  assert configuration_literal == test_config["initialConfiguration"]
+    assert configuration_literal == test_config["initialConfiguration"]
 
-  #  Enum.each(test_config["events"], fn test_case ->
-  #    machine = machine |> Exstate.Machine.send(test_case["event"]["name"])
-  #    assert (machine |> Exstate.Machine.configuration_literal) == test_case["nextConfiguration"]
-  #  end)
-  #end
+    Enum.each(test_config["events"], fn test_case ->
+      machine = machine |> Exstate.Machine.send(test_case["event"]["name"])
+      assert (machine |> Exstate.Machine.configuration_literal) == test_case["nextConfiguration"]
+    end)
+  end
+
+  test "scxml test hier2" do
+    scxml_path = "./test/fixtures/scxml/hierarchy/hier2.scxml"
+    test_path = "./test/fixtures/scxml/hierarchy/hier2.json"
+    {:ok, test_contents} = File.read Path.expand test_path
+    {:ok, test_config} = Poison.decode test_contents
+
+    machine = Exstate.machine_from_file(scxml_path)
+    configuration_literal = machine
+      |> Exstate.Machine.configuration_literal
+
+    assert configuration_literal == test_config["initialConfiguration"]
+
+    Enum.each(test_config["events"], fn test_case ->
+      machine = machine |> Exstate.Machine.send(test_case["event"]["name"])
+      assert (machine |> Exstate.Machine.configuration_literal) == test_case["nextConfiguration"]
+    end)
+  end
 end
