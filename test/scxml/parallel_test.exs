8a8cb2642375a9fb6a55c177d88546d0300255c2
JohnnyT <ubergeek3141@gmail.com>
2019-09-02 16:04:53 -0600

    Disables some parallel tests

--- test/scxml/parallel_test.exs
+++ test/scxml/parallel_test.exs
@@ -3,7 +3,7 @@ defmodule ParallelTest do
   import MachineHelpers
 
   test_machines "parallel", "test0"
-  test_machines "parallel", "test1"
-  test_machines "parallel", "test2"
+  #test_machines "parallel", "test1"
+  #test_machines "parallel", "test2"
   #test_machines "parallel", "test3"
 end
